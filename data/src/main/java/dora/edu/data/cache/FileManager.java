aa32585063eff96b8469a259820dbf65971686c5
dora <khievdora@gmail.com>
2017-07-21 01:04:35 -0500

    Implement cache

---
+++ data/src/main/java/dora/edu/data/cache/FileManager.java
@@ -0,0 +1,8 @@
+package dora.edu.data.cache;
+
+/**
+ * Created by Dora on 7/21/2017.
+ */
+
+public class FileManager {
+}
---
+++ data/src/main/java/dora/edu/data/cache/serializer/Serializer.java
@@ -0,0 +1,8 @@
+package dora.edu.data.cache.serializer;
+
+/**
+ * Created by Dora on 7/21/2017.
+ */
+
+public class Serializer {
+}
